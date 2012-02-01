f6ef3292624ec941c518f39ba45b4860121aecea
myzone <myzone@ubuntu.ubuntu-domain>
2012-02-01 18:37:39 +0300

    file structure was added

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=Syntax_Analyzer
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/Syntax_Analyzer
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=SyntaxAnalyzer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/SyntaxAnalyzer.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=Syntax_Analyzer
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/Syntax_Analyzer
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=SyntaxAnalyzer.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/SyntaxAnalyzer.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * File:   main.cpp
+ * Author: myzone
+ *
+ * Created on 1 February 2012 y., 18:03
+ */
+
+int main(int argc, char** argv) {
+
+}
